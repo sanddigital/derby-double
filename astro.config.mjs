6417e33633beb026d4abe07c6f071f288557975e
Steven Anderson <steven@bbzv7fr3z44vjr090bpg.ip6.superloop.au>
2026-07-22 20:02:43 +1000

    Configure GitHub Pages deployment workflow and base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sanddigital.github.io',
+  base: '/derby-double/',
   output: 'static',
   devToolbar: {
     enabled: false
