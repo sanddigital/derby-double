99cbf14614a70585f3868454ae2359808ed83274
Steven Anderson <steven@bbzv7fr3z44vjr090bpg.ip6.superloop.au>
2026-07-23 20:29:03 +1000

    Configure custom domain derbydouble.au and CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sanddigital.github.io',
-  base: '/derby-double/',
+  site: 'https://derbydouble.au',
   output: 'static',
   devToolbar: {
     enabled: false
